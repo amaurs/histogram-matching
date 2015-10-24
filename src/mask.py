e5eafa89e61165a10f6eb611426ab917e7e0735c
amaurs <amaury.gtz@gmail.com>
2015-10-23 20:55:26 -0500

    Now the script actually uses the mask.

--- src/mask.py
+++ src/mask.py
@@ -16,7 +16,7 @@ def use_mask(original_image, mask):
     print width,height
 
     print bands
-    output_file = "final_final_final.tif"
+    output_file = "final_final_final_test.tif"
     driver = gdal.GetDriverByName('GTiff')
     result_image = driver.Create(output_file, width, height, bands, gdal.GDT_Int16  )
     result_image.SetGeoTransform(geo_transform)
@@ -27,30 +27,27 @@ def use_mask(original_image, mask):
     original_array_green = numpy.ravel(numpy.array(original_dataset.GetRasterBand(2).ReadAsArray()))
     original_array_blue = numpy.ravel(numpy.array(original_dataset.GetRasterBand(3).ReadAsArray()))
 
-    mask_array_red = numpy.ravel(numpy.array(original_dataset.GetRasterBand(1).ReadAsArray()))
-    mask_array_green = numpy.ravel(numpy.array(original_dataset.GetRasterBand(2).ReadAsArray()))
-    mask_array_blue = numpy.ravel(numpy.array(original_dataset.GetRasterBand(3).ReadAsArray()))
+    mask_array_red = numpy.ravel(numpy.array(mask_dataset.GetRasterBand(1).ReadAsArray()))
+    mask_array_green = numpy.ravel(numpy.array(mask_dataset.GetRasterBand(2).ReadAsArray()))
+    mask_array_blue = numpy.ravel(numpy.array(mask_dataset.GetRasterBand(3).ReadAsArray()))
 
     size = len(original_array_red)
     print size
-    final_array_red = numpy.zeros([len(original_array_red)])
-    final_array_green = numpy.zeros([len(original_array_red)])    
-    final_array_blue = numpy.zeros([len(original_array_red)])
+    final_array_red = numpy.ravel(numpy.array(original_dataset.GetRasterBand(1).ReadAsArray()))
+    final_array_green = numpy.ravel(numpy.array(original_dataset.GetRasterBand(2).ReadAsArray()))  
+    final_array_blue = numpy.ravel(numpy.array(original_dataset.GetRasterBand(3).ReadAsArray()))
 
 
 
     for i in range(size):
-    	if(i%10000 == 0):
-    		print i*1.0 / size
+        if(i%100000 == 0):
+            print i*1.0 / size
 
-        if(mask_array_red[i] + mask_array_green[i],mask_array_blue[i] == 0):
+        if (mask_array_red[i] + mask_array_green[i] + mask_array_blue[i]) < 1:
             final_array_red[i] = 0.2126 * original_array_red[i] + 0.7152 * original_array_green[i] + 0.0722 * original_array_blue[i]
             final_array_green[i] = 0.2126 * original_array_red[i] + 0.7152 * original_array_green[i] + 0.0722 * original_array_blue[i]
             final_array_blue[i] = 0.2126 * original_array_red[i] + 0.7152 * original_array_green[i] + 0.0722 * original_array_blue[i]
-        else:
-            final_array_red[i] = original_array_red[i]
-            final_array_green[i] = original_array_green[i]
-            final_array_blue[i] = original_array_blue[i]
+
     result_image.GetRasterBand(1).WriteArray(final_array_red.reshape((width, height)))
     result_image.GetRasterBand(2).WriteArray(final_array_green.reshape((width, height)))
     result_image.GetRasterBand(3).WriteArray(final_array_blue.reshape((width, height)))
